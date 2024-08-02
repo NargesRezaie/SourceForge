66b7acf9b5bcf114d7176b56bc16ebf76babc485
NargesRezaie <130102021+NargesRezaie@users.noreply.github.com>
2024-08-02 21:01:09 +0330

    create navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens:{
+      'phone':'320px',
+      'tablet':'480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+
+    }
   },
   plugins: [],
 }
